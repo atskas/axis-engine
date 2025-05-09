f6da28270d9de848faa0cae04ec4e154a3997a6b
Pan <76873505+atskas@users.noreply.github.com>
2025-05-09 22:46:52 +0300

    Implement window creation and render loop

---
+++ UntitledEngine/Engine.cs
@@ -0,0 +1,41 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
+
+namespace UntitledEngine
+{
+    public class Engine : GameWindow
+    {
+        // Constructor to set up window size and title
+        public Engine(int width, int height, string title)
+            : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = new Vector2i(width, height), Title = title })
+        {
+            // Can do additional setup here
+        }
+
+        // Called once the window is loaded, I believe that's self-explanatory
+        protected override void OnLoad()
+        {
+            // Set the background color (Temporary)
+            GL.ClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        }
+
+        // Called every frame
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+
+        }
+
+        // Called every frame to render the content to the screen
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            // Clear the screen with the background color
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            // Swap the buffers to display frame
+            SwapBuffers();
+        }
+
+    }
+}
---
+++ UntitledEngine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UntitledEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine(800, 600, "Engine");
+            engine.Run();
+        }
+    }
+}
