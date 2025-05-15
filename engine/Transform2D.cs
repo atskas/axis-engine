5490477f64b68fd520b6d4e42c9d3b76a04177b6
Pan <76873505+atskas@users.noreply.github.com>
2025-05-15 20:29:30 +0300

    [ENGINE] Create Transform2D struct for improved entity system

---
+++ engine/Transform2D.cs
@@ -0,0 +1,27 @@
+﻿using OpenTK.Mathematics;
+
+namespace UntitledEngine.engine
+{
+    public struct Transform2D
+    {
+        // Transform properties
+        public Vector2 Position;
+        public float Rotation; // Radians
+        public Vector2 Scale;
+
+        public Transform2D(Vector2 position, float rotation, Vector2 scale)
+        {
+            Position = position;
+            Rotation = rotation;
+            Scale = scale;
+        }
+
+        // Compute model matrix combining position, rotation and scale
+        public Matrix4 GetModelMatrix()
+        {
+            return Matrix4.CreateScale(new Vector3(Scale.X, Scale.Y, 1.0f)) *
+                   Matrix4.CreateRotationZ(Rotation) *
+                   Matrix4.CreateTranslation(new Vector3(Position.X, Position.Y, 0.0f));
+        }
+    }
+}
--- engine/entities/BaseEntity.cs
+++ engine/entities/BaseEntity.cs
@@ -4,10 +4,7 @@ namespace UntitledEngine.engine.entities
 {
     public class BaseEntity
     {
-        // Transform properties
-        public Vector2 Position { get; set; } = Vector2.Zero;
-        public float Rotation { get; set; } = 0f;
-        public Vector2 Size { get; set; } = Vector2.One;
+        public Transform2D Transform = new Transform2D();
 
         public BaseEntity()
         {
@@ -17,7 +14,8 @@ namespace UntitledEngine.engine.entities
         // Runs when a new entity instance is created
         public void OnLoad()
         {
-
+            Transform.Scale = new Vector2(1.5f, 1.5f);
+            Console.WriteLine(Transform.Scale);
         }
 
         // Runs every frame
@@ -25,26 +23,5 @@ namespace UntitledEngine.engine.entities
         {
 
         }
-
-        // Compute model matrix combining position, rotation and scale
-        public Matrix4 GetModelMatrix()
-        {
-            return
-                Matrix4.CreateTranslation(new Vector3(-0.5f * Size.X, -0.5f * Size.Y, 0f)) * // center origin
-                Matrix4.CreateScale(new Vector3(Size.X, Size.Y, 1f)) *
-                Matrix4.CreateRotationZ(Rotation) *
-                Matrix4.CreateTranslation(new Vector3(Position.X, Position.Y, 0f));
-        }
-
-        // Stuff like the transform properties you won't have to
-        // redefine in any entity you create, you'll just have to
-        // give them values.
-        // For example:
-        //  public YourEntity()
-        //  {
-        //      Position = new Vector2(0, 0);
-        //      Size = new Vector2
-        //      Rotation = 0f;
-        //  }
     }
 }
