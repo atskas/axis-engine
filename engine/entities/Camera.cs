2a443aca725ff27f4f76ff5b20db080384321dba
Pan <76873505+atskas@users.noreply.github.com>
2025-05-21 16:47:23 +0300

    [ENGINE] Create `Camera` entity

---
+++ engine/entities/Camera.cs
@@ -0,0 +1,22 @@
+﻿using OpenTK.Mathematics;
+
+namespace UntitledEngine.engine.entities
+{
+    public class Camera : BaseEntity
+    {
+        public float Zoom { get; set; } = 1f;
+
+        public Matrix4 GetViewMatrix()
+        {
+            return Matrix4.CreateTranslation(-Transform.Position.X, -Transform.Position.Y, 0);
+        }
+
+        public Matrix4 GetProjectionMatrix(float aspectRatio)
+        {
+            float width = 2f / Zoom;
+            float height = width / aspectRatio;
+            return Matrix4.CreateOrthographic(width, height, -1, 1);
+        }
+
+    }
+}
