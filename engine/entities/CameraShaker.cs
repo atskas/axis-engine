c7bf816873abcea21be86f63304e6716245fa0e7
Pan <76873505+atskas@users.noreply.github.com>
2025-05-21 17:43:59 +0300

    [GAME] Simplify unnecessarily complex CameraShake
    didn't end up working out..

--- engine/entities/CameraShaker.cs
+++ engine/entities/CameraShaker.cs
@@ -4,41 +4,25 @@ namespace UntitledEngine.engine.entities
 {
     public class CameraShaker
     {
-        private Vector2 originalPosition;
-        private float timer = 0f;
-        private float duration = 0f;
-        private float intensity = 0f;
-        private float decay = 0f;
-
+        private Vector2 originalPos;
         private readonly Random random = new Random();
 
-        public void Shake(ref Vector2 camPos, float intensity, float duration, float deltaTime)
+        public CameraShaker(ref Camera camera)
         {
-            // Start shaking if not already
-            if (timer <= 0f)
-            {
-                originalPosition = camPos;
-                this.intensity = intensity;
-                this.duration = duration;
-                decay = intensity / duration;
-                timer = duration;
-            }
+            originalPos = camera.Transform.Position;
+        }
 
-            // Update if still shaking
-            if (timer > 0f)
-            {
-                float offsetX = (float)(random.NextDouble() * 2 - 1) * this.intensity;
-                float offsetY = (float)(random.NextDouble() * 2 - 1) * this.intensity;
+        public void StartShaking(ref Vector2 camPos, float intensity)
+        {
+            float offsetX = (float)(random.NextDouble() * 2 - 1) * intensity;
+            float offsetY = (float)(random.NextDouble() * 2 - 1) * intensity;
 
-                this.intensity -= decay * deltaTime;
-                timer -= deltaTime;
+            camPos += new Vector2(offsetX, offsetY);
+        }
 
-                camPos = originalPosition + new Vector2(offsetX, offsetY);
-            }
-            else
-            {
-                camPos = originalPosition;
-            }
+        public void StopShaking(ref Vector2 camPos)
+        {
+            camPos = originalPos;
         }
     }
 }
