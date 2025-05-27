91e670f0716bfdd5afe437cb6388f0938413d663
Pan <atskas0@gmail.com>
2025-05-27 23:23:43 +0300

    Clean up Inspector Panel code & beautify

--- Core/Editor/Panels/InspectorPanel.cs
+++ Core/Editor/Panels/InspectorPanel.cs
@@ -1,6 +1,7 @@
 using Box2D.NetStandard.Dynamics.Bodies;
 using ImGuiNET;
 using UntitledEngine.Core.Components;
+using UntitledEngine.Core.Entities;
 
 namespace UntitledEngine.Core.UI;
 
@@ -17,77 +18,60 @@ public class InspectorPanel
             ImGui.End();
             return;
         }
-        
-        var pb = selected.GetComponent<PhysicsBody>(); // Get selected entity's physics body (if it has one)
 
         ImGui.Text($"Name: {selected.Name}");
 
-        var pos = selected.Transform.Position;
-        if (ImGui.DragFloat2("Position", ref pos))
-        {
-            if (pb != null)
-                pb.BodyPosition = pos;
-            else
-                selected.Transform.Position = pos;
-        }
-
-        float rot = selected.Transform.Rotation;
-        if (ImGui.DragFloat("Rotation", ref rot))
-        {
-            if (pb != null)
-                pb.BodyRotation = rot;
-            else
-                selected.Transform.Rotation = rot;
-        }
-        
-        var scale = selected.Transform.Scale;
-        if (ImGui.DragFloat2("Scale", ref scale))
-        {
-            selected.Transform.Scale = scale;
-            if (pb != null)
-            {
-                pb.ShapeScale = selected.Transform.Scale;
-            }
-        }
-        
-        if (pb != null)
-        {
-            var density = pb.Density;
-            if (ImGui.DragFloat("Density", ref density))
-            {
-                pb.Density = density;
-            }
-        }
-        
-        if (pb != null)
-        {
-            var friction = pb.Friction;
-            if (ImGui.DragFloat("Friction", ref friction))
-            {
-                pb.Friction = friction;
-            }
-        }
-
-        if (pb != null)
-        {
-            bool isStatic = pb.BodyType == BodyType.Static;
-            if (ImGui.Checkbox("Static Body", ref isStatic))
-            {
-                pb.BodyType = isStatic ? BodyType.Static : BodyType.Dynamic;
-            }
-        }
-        
-        if (pb != null)
-        {
-            bool rotFixed = pb.FixedRotation;
-            if (ImGui.Checkbox("Fixed Rotation", ref rotFixed))
-            {
-                pb.FixedRotation = rotFixed;
-                if (pb.Body != null)
-                    pb.Body.SetFixedRotation(rotFixed);
-            }
-        }
+        DrawTransformSection(selected);
+        DrawPhysicsBodySection(selected.GetComponent<PhysicsBody>());
 
         ImGui.End();
+    }
+
+    private void DrawTransformSection(Entity entity)
+    {
+        ImGui.Separator();
+        ImGui.Text("Transform");
+
+        var pos = entity.Transform.Position;
+        if (ImGui.DragFloat2("Position", ref pos))
+            entity.Transform.Position = pos;
+
+        var rot = entity.Transform.Rotation;
+        if (ImGui.DragFloat("Rotation", ref rot))
+            entity.Transform.Rotation = rot;
+
+        var scale = entity.Transform.Scale;
+        if (ImGui.DragFloat2("Scale", ref scale))
+            entity.Transform.Scale = scale;
+    }
+
+    private void DrawPhysicsBodySection(PhysicsBody pb)
+    {
+        if (pb == null)
+            return;
+
+        ImGui.Separator();
+        ImGui.Text("Physics");
+
+        // Update ShapeScale from entity scale if needed
+        var shapeScale = pb.ShapeScale;
+        if (ImGui.DragFloat2("Shape Scale", ref shapeScale))
+            pb.ShapeScale = shapeScale;
+
+        float density = pb.Density;
+        if (ImGui.DragFloat("Density", ref density))
+            pb.Density = density;
+
+        float friction = pb.Friction;
+        if (ImGui.DragFloat("Friction", ref friction))
+            pb.Friction = friction;
+
+        bool isStatic = pb.BodyType == BodyType.Static;
+        if (ImGui.Checkbox("Static Body", ref isStatic))
+            pb.BodyType = isStatic ? BodyType.Static : BodyType.Dynamic;
+
+        bool rotFixed = pb.FixedRotation;
+        if (ImGui.Checkbox("Fixed Rotation", ref rotFixed))
+            pb.FixedRotation = rotFixed;
     }
 }
